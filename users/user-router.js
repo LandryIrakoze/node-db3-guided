16e2e96cb5a59eb4248e2f60f16daf5ffb2f3524
Landry Irakoze <irakozel@gmail.com>
2019-09-11 13:50:20 -0400

    slection bug fix

--- users/user-router.js
+++ users/user-router.js
@@ -97,6 +97,7 @@ router.get('/:id/posts', (req, res) => {
   db('posts as p')
     .join('users as u', 'u.id', '=', 'p.user_id')
     .where('u.id', id)
+    .select('p.id', 'p.contents', 'u.username')
     .then(posts => {
       res.status(200).json(posts)
     })
